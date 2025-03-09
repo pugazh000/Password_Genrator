2e9f5ae9944a7bc80f2e72b61c01dd13b88e4647
pugazh000 <pug.octskye@gmail.com>
2025-03-09 19:24:32 +0530

    Update app.py

--- password_Gen/app.py
+++ password_Gen/app.py
@@ -1,35 +1,21 @@
+from flask import Flask, jsonify
 import boto3
 import random
 import string
-from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# AWS DynamoDB setup
 dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-1")
-table_name = "GeneratedPasswords"
-
-# Create the table (Run once manually)
-try:
-    table = dynamodb.create_table(
-        TableName=table_name,
-        KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
-        AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
-        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
-    )
-    table.wait_until_exists()
-except:
-    table = dynamodb.Table(table_name)
+table = dynamodb.Table("GeneratedPasswords")
 
 @app.route("/generate", methods=["GET"])
 def generate_password():
     length = 12
     chars = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(chars) for _ in range(length))
-
-    # Store password in DynamoDB
+    
     table.put_item(Item={"id": str(random.randint(1000, 9999)), "password": password})
-
+    
     return jsonify({"password": password})
 
 @app.route("/passwords", methods=["GET"])
@@ -39,4 +25,4 @@ def list_passwords():
     return jsonify({"generated_passwords": passwords})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
